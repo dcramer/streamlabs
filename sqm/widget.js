5148f5a4a3897656563026e8541c6e7773b46b4d
David Cramer <dcramer@gmail.com>
2022-11-15 19:15:17 -0800

    Draft SQM widget

---
+++ sqm/widget.js
@@ -0,0 +1,30 @@
+let container;
+let host;
+let eyeSensor;
+
+function setText(value) {
+  container.text(value);
+  if (value > 20) {
+    container.addClass('bad');
+  } else {
+    container.removeClass('bad');
+  }
+}
+
+function updateSQM() {
+  fetch(`http://${host}:1380/geteye?idx=${eyeSensor}`).then(response => {
+    const data = response.json();
+    if (data.result === "OK") {
+    	setText(response.value);
+    }
+  });
+  
+  setTimeout(updateSQM, 5000);
+}
+
+window.addEventListener('onWidgetLoad', function (obj) {
+  host = obj.detail.fieldData.host;
+  container = $('.main-container');
+  eyeSensor = 0;
+  updateSQM();
+});
